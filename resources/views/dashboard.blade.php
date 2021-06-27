be9e5409eeada8dbbafcfc7e67b645403a090a87
MohamadSyahrul <mochamadsahrul28@gmail.com>
2021-06-28 01:56:20 +0700

    view login dan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,107 @@
 @extends('layouts.master')
 @section('title')
-   Poliwangi - Buku Alumni <?php echo date("M Y"); ?>
+Poliwangi - Buku Alumni <?php echo date("M Y"); ?>
 @endsection
 @section('content')
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
+<div class="content-overlay"></div>
+<div class="header-navbar-shadow"></div>
+<div class="content-wrapper">
+    <div class="content-header row">
     </div>
-</x-app-layout>
+    <div class="content-body">
+        <!-- Dashboard Ecommerce Starts -->
+        <section id="dashboard-ecommerce">
+            <div class="row match-height">
+                <!-- Medal Card -->
+                <div class="col-xl-4 col-md-6 col-12">
+                    <div class="card card-congratulation-medal">
+                        <div class="card-body">
+                            <h5>Congratulations 🎉 John!</h5>
+                            <p class="card-text font-small-3">You have won gold medal</p>
+                            <h3 class="mb-75 mt-2 pt-50">
+                                <a href="javascript:void(0);">$48.9k</a>
+                            </h3>
+                            <button type="button" class="btn btn-primary">View Sales</button>
+                            <img src="{{asset('admin/app-assets/images/illustration/badge.svg')}}" class="congratulation-medal"
+                                alt="Medal Pic" />
+                        </div>
+                    </div>
+                </div>
+                <!--/ Medal Card -->
+
+                <!-- Statistics Card -->
+                <div class="col-xl-8 col-md-6 col-12">
+                    <div class="card card-statistics">
+                        <div class="card-header">
+                            <h4 class="card-title">Statistics</h4>
+                            <div class="d-flex align-items-center">
+                                <p class="card-text font-small-2 mr-25 mb-0">Updated 1 month ago</p>
+                            </div>
+                        </div>
+                        <div class="card-body statistics-body">
+                            <div class="row">
+                                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                                    <div class="media">
+                                        <div class="avatar bg-light-primary mr-2">
+                                            <div class="avatar-content">
+                                                <i data-feather="trending-up" class="avatar-icon"></i>
+                                            </div>
+                                        </div>
+                                        <div class="media-body my-auto">
+                                            <h4 class="font-weight-bolder mb-0">230k</h4>
+                                            <p class="card-text font-small-3 mb-0">Sales</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                                    <div class="media">
+                                        <div class="avatar bg-light-info mr-2">
+                                            <div class="avatar-content">
+                                                <i data-feather="user" class="avatar-icon"></i>
+                                            </div>
+                                        </div>
+                                        <div class="media-body my-auto">
+                                            <h4 class="font-weight-bolder mb-0">8.549k</h4>
+                                            <p class="card-text font-small-3 mb-0">Customers</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
+                                    <div class="media">
+                                        <div class="avatar bg-light-danger mr-2">
+                                            <div class="avatar-content">
+                                                <i data-feather="box" class="avatar-icon"></i>
+                                            </div>
+                                        </div>
+                                        <div class="media-body my-auto">
+                                            <h4 class="font-weight-bolder mb-0">1.423k</h4>
+                                            <p class="card-text font-small-3 mb-0">Products</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-3 col-sm-6 col-12">
+                                    <div class="media">
+                                        <div class="avatar bg-light-success mr-2">
+                                            <div class="avatar-content">
+                                                <i data-feather="dollar-sign" class="avatar-icon"></i>
+                                            </div>
+                                        </div>
+                                        <div class="media-body my-auto">
+                                            <h4 class="font-weight-bolder mb-0">$9745</h4>
+                                            <p class="card-text font-small-3 mb-0">Revenue</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ Statistics Card -->
+            </div>
+
+        </section>
+        <!-- Dashboard Ecommerce ends -->
+
+    </div>
+</div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,7 +15,7 @@
 <body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
 
     <!-- BEGIN: Header-->
-    @include('layouts.template.header')
+    @include('layouts.header')
     <!-- END: Header-->
 
 
